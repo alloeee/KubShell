c602d4688cc00d520188d497c700f1669d294589
alloeee <artem993.92016@yandex.ru>
2025-10-24 21:49:13 +0300

    Тесты 1-5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-
 #include <iostream>
 
 #include <string>
 
 #include <fstream>
+
 
 
 int main() {
@@ -14,145 +14,118 @@ int main() {
 
     std::cerr << std::unitbuf;
 
-    std::cout<<"$ ";
 
-    const char* home = std::getenv("HOME");
-    std::string historyPath = std::string(home) + "/.kubsh_history";
+
+    //std::cout << "$ ";
+
+
+
+    const char* home=std::getenv("HOME");
+
+    std::string historyPath=std::string(home)+"/.kubsh_history";
+
+
 
     std::string input;
-    while (std::getline(std::cin, input)) {
 
-    
+    while (std::getline(std::cin, input))
+
+    {
+
+        
+
         if(!input.empty()){
 
 	    std::ofstream history(historyPath,std::ios::app);
 
 	    history << input<<"\n";
+
  	}
 
-
-
 	if(input=="history")
+
 	{
+
 	    std::ifstream historyOutput(historyPath);
-        std::string line;
 
-        while(std::getline(historyOutput,line))
+            std::string line;
 
-        {
-            std::cout<<line<<"\n";
+            while(std::getline(historyOutput,line))
+
+            {
+
+                std::cout<<line<<"\n";
+
+            }
+
+	}
+
+        else if (input == "\\q"){
+
+            break;
         }
 
-    }
+        else if (input.substr(0, 7) == "debug '" && input[input.length() - 1] == '\'')
 
-    else if (input == "\\q") {
-        break;
-    }
-
-    else if (input.substr(0, 7) == "debug '" && input[input.length() - 1] == '\''){
+        {
 
         std::cout << input.substr(7, input.length() - 8) << std::endl;    
-    
-    }
 
-    else if (input.substr(0,4) == "\\e $")
+        }
 
+else if (input.substr(0,4) == "\\e $")
+{
+    std::string varName = input.substr(4);
+    const char* value = std::getenv(varName.c_str());
+
+   if(value != nullptr)
     {
-
-        std::string varName = input.substr(4);
-
-        const char* value = std::getenv(varName.c_str());
-
-
-
-    if(value != nullptr)
-
+        std::string valueStr = value;
+        
+        bool has_colon = false;
+        for (char c : valueStr)
         {
-
-            std::string valueStr = value;//Преобразуем C-строку в C++ строку
-
-
-
-            bool has_colon = false;//Флаг для проверки наличия двоеточий
-
+            if (c == ':') 
+            {
+                has_colon = true;
+                break;
+            }
+        }
+        
+        if (has_colon) 
+        {
+            std::string current_part = "";
             for (char c : valueStr)
-
             {
-
                 if (c == ':') 
-
                 {
-
-                    has_colon = true;
-
-                    break;
-
+                    std::cout << current_part << "\n";
+                    current_part = "";
                 }
-
-            }
-
-
-
-            if (has_colon) 
-
-            {
-
-                std::string current_part = "";//Временная строка для накопления текущей части пути
-
-                for (char c : valueStr)//Разбиваем строку по двоеточиям
-
+                else 
                 {
-
-                    if (c == ':') 
-
-                    {
-
-
-                        std::cout<< current_part << "\n";//Когда встречаем двоеточие - выводим накопленную часть
-
-                        current_part = "";//Сбрасываем временную строку для следующей части
-
-                    }
-
-                    else 
-
-                    {
-
-                        current_part += c;//Иначе добавляем символ к строке
-
-                    }
-
+                    current_part += c;
                 }
-
-
-                std::cout<< current_part << "\n";//Выводим последнюю часть (после последнего двоеточия)
-
-
             }
-
-            else 
-
-            { 
-
-                std::cout << valueStr << "\n";//Если двоеточий нет - просто выводим значение как есть
-
-            }
-
+            std::cout << current_part << "\n";
         }
-
-        else
-
-        {
-
-            std::cout << varName << ": не найдено\n";
-
+        else 
+        { 
+            std::cout << valueStr << "\n";
         }
-
     }
+    else
+    {
+        std::cout << varName << ": не найдено\n";
+    }
+}
 
-    else std::cout<<input<<": command not found\n";
 
-    std::cout << "$ ";
+
+	else std::cout<<input<<": command not found\n";
+
+
+	//std::cout<<"$ ";
 
     }
 
