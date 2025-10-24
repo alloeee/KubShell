91fc34d37b10a8b6030d2c7e787b4ba280824d8d
alloeee <artem993.92016@yandex.ru>
2025-10-24 21:30:13 +0300

    Задание 7.Добавим команду по выводу переменной окружения (\e /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin, списком, если есть :)

--- main.cpp
+++ main.cpp
@@ -55,6 +55,101 @@ int main() {
     
     }
 
+    else if (input.substr(0,4) == "\\e $")
+
+    {
+
+        std::string varName = input.substr(4);
+
+        const char* value = std::getenv(varName.c_str());
+
+
+
+    if(value != nullptr)
+
+        {
+
+            std::string valueStr = value;//Преобразуем C-строку в C++ строку
+
+
+
+            bool has_colon = false;//Флаг для проверки наличия двоеточий
+
+            for (char c : valueStr)
+
+            {
+
+                if (c == ':') 
+
+                {
+
+                    has_colon = true;
+
+                    break;
+
+                }
+
+            }
+
+
+
+            if (has_colon) 
+
+            {
+
+                std::string current_part = "";//Временная строка для накопления текущей части пути
+
+                for (char c : valueStr)//Разбиваем строку по двоеточиям
+
+                {
+
+                    if (c == ':') 
+
+                    {
+
+
+                        std::cout<< current_part << "\n";//Когда встречаем двоеточие - выводим накопленную часть
+
+                        current_part = "";//Сбрасываем временную строку для следующей части
+
+                    }
+
+                    else 
+
+                    {
+
+                        current_part += c;//Иначе добавляем символ к строке
+
+                    }
+
+                }
+
+
+                std::cout<< current_part << "\n";//Выводим последнюю часть (после последнего двоеточия)
+
+
+            }
+
+            else 
+
+            { 
+
+                std::cout << valueStr << "\n";//Если двоеточий нет - просто выводим значение как есть
+
+            }
+
+        }
+
+        else
+
+        {
+
+            std::cout << varName << ": не найдено\n";
+
+        }
+
+    }
+
     else std::cout<<input<<": command not found\n";
 
     std::cout << "$ ";
